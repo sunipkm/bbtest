14adb61a460db36d8922d3e7627d21f35a8856c4
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2020-10-08 20:52:08 +0000

    1. Updated to self-execute, 2. Added debug messages, 3. Transmitting on both channels

--- tx.py
+++ tx.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 import adi
 
@@ -11,7 +12,7 @@ signal.signal(signal.SIGINT, sighandler)
 
 # get data
 dat = np.loadtxt('bb_out.txt', dtype = np.uint32)
-
+print("Loaded data")
 # real and imag buffer
 re = np.zeros(dat.shape, dtype = np.int16)
 im = re.copy()
@@ -20,22 +21,23 @@ im = re.copy()
 for i in range(dat.shape[0]):
     re[i] = 0xffff & (dat[i] >> 16)
     im[i] = 0xffff & (dat[i])
-
+print("Split data into re and im")
 # recombine into complex datastream
 data = re + 1j*im 
-
+print("Created complex IQ stream")
+# initialize radio
 sdr = adi.ad9361('local:')
-
-sdr.filter = 'modem_filter.ftr'
-
+print("Started radio")
+sdr.filter = '/home/sunip/modem_filter.ftr'
+print("Loaded filter")
 sdr.sample_rate = 20000000 # 20 MHz
 sdr.tx_rf_bandwidth = 17650790
 sdr.tx_lo = 2400000000 # 2.4 GHz
 sdr.tx_cyclic_buffer = True
 sdr.tx_hardwaregain = -10 # -10 dB attn
 
-sdr.tx(data)
-
+sdr.tx([data, data])
+print("Transmission started, press Ctrl + C to exit")
 signal.pause() # wait for release
 
 sdr.tx_destroy_buffer()
